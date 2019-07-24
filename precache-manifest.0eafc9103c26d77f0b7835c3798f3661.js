6156cbea6b8d47cc69cd9b82e4acdf3e1528945f
Jaryd Krishnan <jaryd@harvestprofit.com>
2019-07-23 21:18:14 -0500

    Updates

---
+++ precache-manifest.0eafc9103c26d77f0b7835c3798f3661.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0be4d92f73739db41d9a8848d71e0341",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9bf64fc796e03fcfd58",
+    "url": "/static/js/2.50bf3924.chunk.js"
+  },
+  {
+    "revision": "96ea89d3837f095c6065",
+    "url": "/static/js/main.03cb40ee.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
